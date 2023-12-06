2cfd8bc91838d3dbe0dd92da81093fa524795763
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-12-06 15:49:34 -0500

    juiygtfrdx

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Dec 2023
+ * This program sends bluetooth messages
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+radio.setGroup(1)
+
+while (true) {
+  if (input.buttonIsPressed(Button.A) === true) {
+    // find distance from sonar
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+      DigitalPin.P1,
+      DigitalPin.P2,
+      PingUnit.Centimeters
+    ) 
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+    if (distanceToObject <= 10) {
+      radio.sendString("Too Close")
+    }
+  }
+  radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+  })
+}
