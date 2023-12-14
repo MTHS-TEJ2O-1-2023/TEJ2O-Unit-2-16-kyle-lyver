9067899e6d128dc3cb76b95949e856edd85e60e8
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-12-14 18:06:14 +0000

    pythjone

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,6 +1,6 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Kyle Lyver
+Created on: Dec 2023
 This module is a Micro:bit MicroPython program
 """
 
@@ -12,23 +12,22 @@ import random
 from microbit import display, Image, button_a, sleep
 
 # Create the "flash" animation frames. Can you work out how it's done?
-flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
-# Event loop.
+flash = [Image().invert() * (i / 9) for i in range(9, -1, -1)]
 
+radio.on()
+display.show(Image.HAPPY)
+# Event loop.
 while True:
-# Button A sends a "flash" message.
-if button_a.was_pressed():
-    radio.send('flash') # a-ha
-# Read any incoming messages.
-incoming = radio.receive()
-if incoming == 'flash':
-    # If there's an incoming "flash" message display
-    # the firefly flash animation after a random short
-    # pause.
-    sleep(random.randint(50, 350))
-    display.show(flash, delay=100, wait=False)
-    # Randomly re-broadcast the flash message after a
-    # slight delay.
-    if random.randint(0, 9) == 0:
-    sleep(500)
-    radio.send('flash') # a-ha
+    # Button A sends a "flash" message.
+    if button_a.was_pressed():
+        radio.send("flash")
+    # Read any incoming messages.
+    incoming = radio.receive()
+    if incoming == "flash":
+        # If there's an incoming "flash" message display
+        display.show(flash, delay=100, wait=False)
+        # Randomly re-broadcast the flash message after a
+        # slight delay.
+        if random.randint(0, 9) == 0:
+            sleep(500)
+            radio.send("flash")
